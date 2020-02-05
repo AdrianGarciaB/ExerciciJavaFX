81c2438c1e4acfc801fefe5b376dcf79a88bdcdd
Adrián García <adriangarciabel@gmail.com>
2020-02-05 19:30:58 +0100

    Okey

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -2,16 +2,30 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class Controller {
+public class Controller implements Initializable {
     @FXML
     Button button1;
+
+    @FXML
+    MenuBar mainmenu;
 
     @FXML
     private void handleButton1Click(ActionEvent actionEvent){
         System.out.println("Hola " + ((Button) actionEvent.getSource()).getId());
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        mainmenu.setUseSystemMenuBar(true);
+        System.out.println("Hello world!");
+    }
 }
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -2,9 +2,14 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -15,16 +20,23 @@ public class Main extends Application {
         FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource(
                 "Main.fxml"));
         Parent root = (Parent) loader.load();
+        //PersonEditorCtrl ctrl = loader.getController();
+
+        //Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+
 
         // el scene contiene al ROOT NODE
-        Scene scene = new Scene(root, 1920, 1080);
+        Scene scene = new Scene(root, 300, 250);
 
         // configurar y mostrar el stage
         primaryStage.setTitle("Hola JavaFX 8");
         primaryStage.setScene(scene);
         primaryStage.show();
 
-
+        Controller controller = (Controller) loader.getController();
+        /*Button button1 = controller.button1;
+        button1.setOnAction(actionEvent -> System.out.println("test por aki"));
+        button1.setText("Hello world :D!");*/
 
 
     }

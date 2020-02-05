fa6c9ad842a71a7a63cd36c6b137ab01a827c975
Adrián García <adriangarciabel@gmail.com>
2020-02-05 19:04:39 +0100

    Basic APP

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
 public class Controller {
+    @FXML
+    Button button1;
+
+    @FXML
+    private void handleButton1Click(ActionEvent actionEvent){
+        System.out.println("Hola " + ((Button) actionEvent.getSource()).getId());
+    }
+
 }
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -4,20 +4,34 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource(
+                "Main.fxml"));
+        Parent root = (Parent) loader.load();
+
+        // el scene contiene al ROOT NODE
+        Scene scene = new Scene(root, 1920, 1080);
+
+        // configurar y mostrar el stage
+        primaryStage.setTitle("Hola JavaFX 8");
+        primaryStage.setScene(scene);
         primaryStage.show();
+
+
+
+
     }
 
 
+
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 }
